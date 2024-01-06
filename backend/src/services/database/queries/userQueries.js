3facebcf5130d9e8977ba293676b57820be0a0a7
Jiovany VINCENT <46725100+JiovanyVincent@users.noreply.github.com>
2024-01-06 04:28:48 +0100

    ✨ Add userQueries

---
+++ backend/src/services/database/queries/userQueries.js
@@ -0,0 +1,15 @@
+const { client } = require('../../config/db');
+const database = process.env.DATABASE;
+
+const getAllUsers = async () => {
+  try {
+    const users = client.db(database).collection('users');
+    const result = await users.findOne();
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error("Erreur lors de la récupération de l'utilisateur:", error);
+  }
+}
+
+module.exports = { getAllUsers };
