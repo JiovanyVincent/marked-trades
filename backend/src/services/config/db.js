2884a2540bc8267949a4b42f299e370f6e4b0543
Jiovany VINCENT <46725100+JiovanyVincent@users.noreply.github.com>
2024-01-06 08:50:01 +0100

    🗃️ Update database config. replace mongodb native module by mongoose

--- backend/src/services/config/db.js
+++ backend/src/services/config/db.js
@@ -1,19 +1,20 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 
 const credentials = {
   username: process.env.USERNAME,
   dbPassword: process.env.DB_PASSWORD,
   cluster: process.env.CLUSTER,
+  db: process.env.DATABASE
 };
 
-const uri = `mongodb+srv://${credentials.username}:${credentials.dbPassword}@${credentials.cluster}.g1fjuwb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${credentials.username}:${credentials.dbPassword}@${credentials.cluster}.g1fjuwb.mongodb.net/${credentials.db}?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connecté à la base de données via Mongoose');
+  })
+  .catch((error) => {
+    console.error('Erreur de connexion :', error);
+  });
 
-module.exports = { client };
+module.exports = { mongoose };
